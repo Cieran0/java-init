9ac4b9622b0316e0f661ae502c0494730287cfc5
Cieran0 <cieran999@gmail.com>
2024-08-20 18:15:14 +0100

    Make a java project by "java-init ProjectName"

---
+++ java-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage:"
+    echo "$0 [name]"
+    exit 1
+fi
+
+NAME_GIVEN=$1
+
+mkdir src
+echo -e "Manifest-Version: 1.5\nMain-Class: $NAME_GIVEN\nCreated-By: 1.0 (java-init)" > MANIFEST.MF
+
+echo -e "public class $NAME_GIVEN {\n\n    public static void main(String[] args) {\n\n    }\n\n}" > "src/$NAME_GIVEN.java"
+echo -e "build:\n	javac -d src src/*.java\n	cd src; \\" > "Makefile"
+echo -e "    jar cmvf ../MANIFEST.MF ../Gamerr.jar *.class\n\nrun: build\n	java -jar Gamerr.jar" >> "Makefile"
